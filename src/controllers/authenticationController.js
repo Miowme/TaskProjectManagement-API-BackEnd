a3f860035a1e46e222222648095e8abcb1c5ecfe
William Susanto <williamsusantolukman@gmail.com>
2026-08-11 13:13:55 +0700

    chore: untrack .env file and enforce gitignore

---
+++ src/controllers/authenticationController.js
@@ -0,0 +1,45 @@
+import User from "../models/User.js";
+import { formatJoiErrors } from "../utils/FormatJoiErrors.js";
+import { registerValidation } from "../validations/authenticationValidation.js";
+import bcrypt from "bcrypt";
+
+export const register = async (req, res) => {
+  const { error } = registerValidation.validate(req.body, {
+    abortEarly: false,
+  });
+
+  if (error) {
+    const errors = formatJoiErrors(error);
+    return res.status(400).json({
+      message: "Validation errors",
+      errors,
+    });
+  }
+
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.FindOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "Email already exists",
+      });
+    }
+
+    const user = new User({ name, email, password });
+    const hashedPassword = await bycrypt.hash(password, 10);
+    user.password = hashedPassword;
+    await user.save();
+
+    return res.status(201).json({
+      message: "User registered successfully",
+      user,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
--- src/main.js
+++ src/main.js
@@ -1,9 +1,29 @@
 import express from "express";
 import "dotenv/config";
+import cors from "cors";
+import dns from "dns";
+import cookieParser from "cookie-parser";
+import connectionDB from "./config/db.js";
+import router from "./routes/index.js";
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api", router);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
+  connectionDB();
   console.log(`Server is running on PORT ${PORT}`);
 });
